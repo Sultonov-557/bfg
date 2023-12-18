32ebe53149e20e84a0a79f9b077351963731edeb
Sultonov557 <sultonovzerifboy@gmail.com>
2023-12-18 20:07:35 +0500

    .

--- src/route/user/user.route.ts
+++ src/route/user/user.route.ts
@@ -1,6 +1,5 @@
 import { Composer } from "grammy";
 import { NewContext } from "../../common/types/NewContext.type";
-import { AuthGuard } from "../../middleware/AuthGuard";
 import { UserService } from "./user.service";
 
 export const UserRoute = new Composer<NewContext>();
